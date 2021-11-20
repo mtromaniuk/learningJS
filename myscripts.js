791ec0d3670076d2127f1d701390bce8e1d7b2dc
mromaniuk <mt.romaniuk@gmail.com>
2021-11-20 13:12:15 +0100

    rebased

--- myscripts.js
+++ myscripts.js
@@ -96,6 +96,138 @@ calcAddNumbers.onclick = function calculate() {
         }
 }
 
+addBtn.onclick = function() {
+    console.log(firstNumber.value, secondNumber.value, "AddBtn");
+    if(firstNumber.value=="" || secondNumber.value=="") {
+        console.log('You not have any added numbers. Fix it before you wil wish calculate them.')
+        resultText.textContent = `Error. You must have added numbers. Please, refresh page and try again.`;
+    } else {
+        countContainers++;
+        arrayIndexContainers.push(countContainers);
+        console.log(arrayIndexContainers);
+
+        let resultsAdd = parseInt(firstNumber.value) + parseInt(secondNumber.value);
+        let addNewContainer = document.createElement("div");
+        let contAdd = document.createElement("div");
+        let deleteContainer = document.createElement('button');
+
+        deleteContainer.id = "deleteContainerBtn";
+        deleteContainer.textContent = "X";
+        addNewContainer.className = "container--result";
+        containerResults.appendChild(addNewContainer);
+        addNewContainer.appendChild(deleteContainer);
+        addNewContainer.appendChild(resultDiv);
+        addNewContainer.appendChild(contAdd);
+        contAdd.textContent = `Sum: ${firstNumber.value} + ${secondNumber.value} = ${resultsAdd.toFixed(2)}`;
+        firstNumber.value = "";
+        secondNumber.value = "";
+
+        deleteContainer.onclick = function(countContainers) {
+            addNewContainer.remove();
+            arrayIndexContainers.splice(countContainers, 1);
+        }
+    }
+}
+
+subBtn.onclick = function() {
+    console.log(firstNumber.value, secondNumber.value, "SubBtn")
+    if(firstNumber.value=="" || secondNumber.value=="") {
+        console.log('You not have any added numbers. Fix it before you wil wish calculate them.')
+        resultText.textContent = `Error. You must have added numbers. Please, refresh page and try again.`;
+    } else {
+        countContainers++;
+        arrayIndexContainers.push(countContainers);
+        console.log(arrayIndexContainers);
+        let resultsSub = parseInt(firstNumber.value) - parseInt(secondNumber.value);
+        let addNewContainer = document.createElement("div");
+        let contSub = document.createElement("div");
+        let deleteContainer = document.createElement('button');
+
+        deleteContainer.id = "deleteContainerBtn";
+        deleteContainer.textContent = "X";
+        addNewContainer.className = "container--result";
+        containerResults.appendChild(addNewContainer);
+        addNewContainer.appendChild(deleteContainer);
+        addNewContainer.appendChild(resultDiv);
+        addNewContainer.appendChild(contSub);
+        contSub.textContent = `Sub: ${firstNumber.value} - ${secondNumber.value} = ${resultsSub.toFixed(2)}`;
+        firstNumber.value = "";
+        secondNumber.value = "";
+
+        deleteContainer.onclick = function(countContainers) {
+            addNewContainer.remove();
+            arrayIndexContainers.splice(countContainers, 1);
+        }
+    }
+}
+
+diviBtn.onclick = function() {
+    console.log(firstNumber.value, secondNumber.value, "DiviBtn");
+    if(firstNumber.value=="" || secondNumber.value=="") {
+        console.log('You not have any added numbers. Fix it before you wil wish calculate them.')
+        resultText.textContent = `Error. You must have added numbers. Please, refresh page and try again.`;
+    } else {
+        countContainers++;
+        arrayIndexContainers.push(countContainers);
+        console.log(arrayIndexContainers);
+
+        let resultsDivide = parseInt(firstNumber.value) / parseInt(secondNumber.value);
+        let addNewContainer = document.createElement("div");
+        let contDivi = document.createElement("div");
+        let deleteContainer = document.createElement('button');
+
+        deleteContainer.id = "deleteContainerBtn";
+        deleteContainer.textContent = "X";
+        addNewContainer.className = "container--result";
+        containerResults.appendChild(addNewContainer);
+        addNewContainer.appendChild(deleteContainer);
+        addNewContainer.appendChild(resultDiv);
+        addNewContainer.appendChild(contDivi);
+        contDivi.textContent = `Divide: ${firstNumber.value} / ${secondNumber.value} = ${resultsDivide.toFixed(2)}`;
+        firstNumber.value = "";
+        secondNumber.value = "";
+
+        deleteContainer.onclick = function(countContainers) {
+            addNewContainer.remove();
+            arrayIndexContainers.splice(countContainers, 1);
+        }
+    }
+}
+
+mulBtn.onclick = function() {
+    console.log(firstNumber.value, secondNumber.value, "MulBtn")
+    if(firstNumber.value=="" || secondNumber.value=="") {
+        console.log('You not have any added numbers. Fix it before you wil wish calculate them.')
+        resultText.textContent = `Error. You must have added numbers. Please, refresh page and try again.`;
+    } else {
+        countContainers++;
+        arrayIndexContainers.push(countContainers);
+        console.log(arrayIndexContainers);
+        let resultsMulti = parseFloat(firstNumber.value) * parseFloat(secondNumber.value);
+        let addNewContainer = document.createElement("div");
+        let contMult = document.createElement("div");
+        let deleteContainer = document.createElement('button');
+
+        deleteContainer.id = "deleteContainerBtn";
+        deleteContainer.textContent = "X";
+        addNewContainer.className = "container--result";
+        containerResults.appendChild(addNewContainer);
+        addNewContainer.appendChild(deleteContainer);
+        addNewContainer.appendChild(resultDiv);
+        addNewContainer.appendChild(contMult);
+        contMult.textContent = `Multi: ${firstNumber.value} * ${secondNumber.value} = ${resultsMulti.toFixed(2)}`;
+        firstNumber.value = "";
+        secondNumber.value = "";
+
+        deleteContainer.onclick = function(countContainers) {
+            addNewContainer.remove();
+            arrayIndexContainers.splice(countContainers, 1);
+        }
+    }
+}
+
+
+
 let numberOfClick = 1;
 let rolledDigit;
 let rolledSecondDigit;
